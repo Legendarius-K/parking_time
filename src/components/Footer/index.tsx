cc6f2e802cafd675a4e0d5196d0b64ca331a022a
Tim Deckel <Tim.Deckel@gmail.com>
2024-05-31 13:34:57 +0200

    added some styling to the footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,16 +2,26 @@
 
 const Footer = () => {
     return (
-        <footer className="h-24 p-2 bg-green-400">
+        <footer className="p-20 bg-pt-primary text-white gap-5 flex flex-col">
             <div className="flex justify-between">
-                <div className="flex">
+                <div className="flex gap-16">
                     <div className="">icon</div>
-                    <div className="">adress</div>
-                    <div className="">contact</div>
+                    <div className="flex-col">
+                        <p className="">adress</p>
+                        <p className="">Nyköpingsvägen 52 | 611 50 NYKÖPING</p>
+                        </div>
+                    <div className="flex-col">
+                        <p className="">contact</p>
+                        <p className="">+46 72 722 44 00 info@parkingtime.se</p>
+                    </div>
                 </div>
-                <div className="flex">
+                <div className="flex-col ">
                     <div className="">links</div>
-                    <div className="">link1 link2 link3</div>
+                    <div className="flex gap-6">
+                        <p className="">Contact</p>
+                        <p className="">About us</p>
+                        <p className="">News</p>
+                    </div>
                 </div>
             </div>
             <div className="border-solid border-0 border-t">
