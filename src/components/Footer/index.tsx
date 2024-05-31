4b351e227527a4a26efd9bc4ad862b6b4fe434be
Tim Deckel <Tim.Deckel@gmail.com>
2024-05-31 13:00:17 +0200

    further expanded the footer structure

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,10 +2,24 @@
 
 const Footer = () => {
     return (
-        <footer className="h-24 bg-green-400">
-            <div className=""></div>
-            <div className=""></div>
-            <div className=""></div>
+        <footer className="h-24 p-2 bg-green-400">
+            <div className="flex justify-between">
+                <div className="flex">
+                    <div className="">icon</div>
+                    <div className="">adress</div>
+                    <div className="">contact</div>
+                </div>
+                <div className="flex">
+                    <div className="">links</div>
+                    <div className="">link1 link2 link3</div>
+                </div>
+            </div>
+            <div className="border-solid border-0 border-t">
+                <div className="flex justify-between">
+                    <div className="">copyright</div>
+                    <div className="">privacy policy</div>
+                </div>
+            </div>
         </footer>
     )
 };
