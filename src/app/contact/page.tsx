2577391196da86f63cd958b934ba44a7f53d792c
Tim Deckel <Tim.Deckel@gmail.com>
2024-06-01 11:56:09 +0200

    basic structure for the contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,7 +3,61 @@
 const Contact = () => {
     return (
         <>
-            Contact   
+            <div className="mt-[80px]">
+                <div className="">
+
+                </div>
+                <div className="">
+                    <div className="">
+                        <div className="">
+                            <h3>Contact</h3>
+                        </div>
+                        <div className="">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, nisi dolorem! Vitae voluptatibus repudiandae doloribus nam eius hic aspernatur! Aut cupiditate natus unde explicabo pariatur officiis sint vitae amet voluptatibus!</p>
+                        </div>
+                    </div>
+                    <div className="">
+                        <div className="">
+                            <a href="">info@parkingtime.se</a>
+                        </div>
+                            <a href="">+46 72 399 15 15 </a>
+                        <div className="">
+
+                        </div>
+                            <a href="">Nyköpingsvägen 52 | 611 50 NYKÖPING</a>
+                        <div className="">
+                            <div className="">
+                                <p>First and last name</p>
+                                <input type="text" name="" id="" />
+                            </div>
+                            <div className="">
+                                <p>Job title</p>
+                                <input type="text" name="" id="" />
+                            </div>
+                            <div className="">
+                                <p>Email</p>
+                                <input type="text" name="" id="" />
+                            </div>
+                            <div className="">
+                                <p>Phone number</p>
+                                <input type="text" name="" id="" />
+                            </div>
+                            <div className="">
+                                <p>Reason for contact</p>
+                                <input type="text" name="" id="" />
+                            </div>
+                            <div className="">
+                                <p>Write us a message</p>
+                                <input type="text" name="" id="" />
+                            </div>
+                            <div className="">
+                                <input type="checkbox" name="checkbox" id="" />
+                            </div>
+                            <button>Contact us</button>
+                        </div>
+                    </div>
+                </div>
+            </div>  
         </>
     )
 };
