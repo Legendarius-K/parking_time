dc3cfb8ea592dc4f78fc0ced7e67e3067003722d
Lisette Svan <svanlisette@gmail.com>
2024-06-03 10:32:25 +0200

    made changes, waiting for right image. will send this to code review

--- src/components/Contact/Hero/index.tsx
+++ src/components/Contact/Hero/index.tsx
@@ -1,13 +1,13 @@
 const Hero = () => {
     return (
         <>
-        <div>
-            <img className="h-80 w-full" src="/Images/pink.jpeg" alt="hero picture" />
-        <div className="absolute inset-0 flex-col items-center justify-center my-48 mx-20">
-            <p className="text-white">Contact</p>
-            <h1 className="text-4xl text-white w-80 mt-4">We would love to hear from you!</h1>
-        </div>
-        </div>
+            <div className="relative">
+                <img className="h-80 w-full md:h-96" src="/Images/pink.jpeg" alt="hero picture" />
+                <div className="absolute inset-0 flex flex-col items-start justify-center my-24 md:my-48 mx-4 sm:mx-10 md:mx-20">
+                    <p className="text-white text-base sm:text-lg md:text-xl">Contact</p>
+                    <h1 className="text-2xl sm:text-3xl md:text-4xl text-white mt-4 w-60 sm:w-80 md:w-100">We would love to hear from you!</h1>
+                </div>
+            </div>
         </>
     );
 }
