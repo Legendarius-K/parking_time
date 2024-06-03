ae20b77982e55effe8027d79caee32ed00d77530
Rob <robert.chamberlain@cmeducations.se>
2024-06-03 13:08:17 +0200

    Safety commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
+import createNextIntlPlugin from 'next-intl/plugin';
+ 
+const withNextIntl = createNextIntlPlugin();
+ 
+/** @type {import('next').NextConfig} */
 const nextConfig = {};
-
-export default nextConfig;
+ 
+export default withNextIntl(nextConfig);
---
+++ src/app/[locale]/page.tsx
@@ -0,0 +1,15 @@
+import {useTranslations} from 'next-intl';
+
+import Image from "next/image";
+
+
+
+export default function Home() {
+    const t = useTranslations('Index');
+    return (
+        <main className="">
+            Homepage jjj
+            <h1>{t('title')}</h1>;
+        </main>
+    );
+}
