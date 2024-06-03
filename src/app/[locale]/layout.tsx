7442275bbb84a919e1d96ef4d69f3b9da9913f6c
Darius K <darius.kaya@gmail.com>
2024-06-03 11:51:17 +0200

    bug testing vercel bug

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -36,6 +36,7 @@ export default async function LocaleLayout({
                     </section>
                     <Footer />
                 </NextIntlClientProvider>
+                <p>HELLO</p>
             </body>
         </html>
     );
