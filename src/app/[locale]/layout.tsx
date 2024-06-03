7bf35e1e7d3f847add25d9326ee5925117d498bb
Darius K <darius.kaya@gmail.com>
2024-06-03 14:01:04 +0200

    bug testing vercel bug back to #5

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -29,13 +29,13 @@ export default async function LocaleLayout({
                 <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
             </head>
             <body className={`${'font-sans font-thin flex flex-col min-h-full'}`}>
+                <NextIntlClientProvider messages={messages}>
                     {/* <Header /> */}
-                {/* <NextIntlClientProvider messages={messages}> */}
                     <section className="grow">
                         {children}
                     </section>
-                {/* </NextIntlClientProvider> */}
                     <Footer />
+                </NextIntlClientProvider>
             </body>
         </html>
     );
