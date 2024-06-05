14a09e1165d56896ffcabc30fc059d09336cfa59
Darius K <darius.kaya@gmail.com>
2024-06-05 11:40:13 +0200

    -

--- src/components/How/index.tsx
+++ src/components/How/index.tsx
@@ -15,7 +15,7 @@ const How = () => {
 
     return (
         <div className='bg-pt-background flex justify-center'>
-            <section className="max-w-[1350px] py-16 md:px-10 flex flex-col items-center w-[70%] md:w-full ">
+            <section className="max-w-[1350px] py-16 md:px-10 flex flex-col items-center w-full sm:w-[70%] md:w-full ">
                 <h2 className="text-center text-3xl md:text-[2.5rem] m-6">Hur det fungerar</h2>
                 <div className="flex flex-col md:flex-row m-8 justify-between items-center">
                     <HowBox srcIcon={ptPen} altIcon='A Pen' number='01' heading='Fyll i registreringsnummer' text='Öppna appen, registrera dig med ditt registreringsnummer.' />
@@ -32,11 +32,13 @@ const How = () => {
 
                     <HowBox srcIcon={ptClock} altIcon='A Clock' number='03' heading='Starta din digitala p-skiva' text='När du har hittat din parkeringszon kan du se över parkeringens regler, starta parkering och stänga appen.' />
                 </div>
-                {width && width > 960 ? (
-                    <Button targetBlank={true} route={'https://apps.apple.com/se/app/parking-time/id1611019108'} btnText='Ladda ner appen' colors='bg-black text-white' />
-                ) : (
-                    <ButtonMobile targetBlank={true} route={'https://apps.apple.com/se/app/parking-time/id1611019108'} btnText='Ladda ner appen' colors='bg-black text-white' />
-                )}
+                <div className='mt-9'>
+                    {width && width > 960 ? (
+                        <Button targetBlank={true} route={'https://apps.apple.com/se/app/parking-time/id1611019108'} btnText='Ladda ner appen' colors='bg-black text-white' />
+                    ) : (
+                        <ButtonMobile targetBlank={true} route={'https://apps.apple.com/se/app/parking-time/id1611019108'} btnText='Ladda ner appen' colors='bg-black text-white' />
+                    )}
+                </div>
             </section>
         </div>
     );
