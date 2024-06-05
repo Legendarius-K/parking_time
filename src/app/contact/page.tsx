8fb2656d7ef6fec020d15d2b2c816739969e33dd
timdeckel <timdeckel@gmail.com>
2024-06-05 14:11:33 +0200

    still working on the checkbox

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -9,6 +9,12 @@ import { useEffect, useState } from "react";
 
 const Contact = () => {
 
+    const [ischecked, toggleCheck] = useState(false);
+
+    const handleToggle = () => {
+
+    }
+
     const [openAccordion, toggleAccordion] = useState(false);
 
     const handleClick = () => {
@@ -21,41 +27,41 @@ const Contact = () => {
 
     return (
         <>
-          <div className="flex flex-col mt-[80px] pt-[100px] px-[30px] pb-[80px] font-mono gap-[50px] text-pt-darkblue1">
-            <div className="flex flex-col gap-5">
+          <div className="flex flex-col mt-[80px] pt-[100px] px-[30px] pb-[80px] font-mono gap-[70px] text-pt-darkblue1">
+            <div className="flex flex-col gap-7">
                 <p className="text-5xl font-sans font-bold text-pt-secondary">Contact us</p>
-                <p className="text-base font-medium text-pt-darkblue2">Whether you have a question, feedback, or need assistance, please fill out the form below, and our team will get back to you as soon as possible.</p>
+                <p className="text-base font-semibold text-pt-darkblue2 leading-8">Whether you have a question, feedback, or need assistance, please fill out the form below, and our team will get back to you as soon as possible.</p>
                 <div className="flex flex-col gap-4 text-pt-darkblue1">
-                    <div className="flex flex-row gap-4 ">
+                    <div className="flex flex-row gap-4 mt-[10px]">
                         <Image src={EmailIcon} alt="Email icon"/>
-                        <a className="underline font-medium" href="">info@parkingtime.se</a>
+                        <a className="underline font-semibold" href="">info@parkingtime.se</a>
                     </div>
                     <div className="flex flex-row gap-4">
                         <Image src={PhoneIcon} alt="Phone icon"/>
-                        <a className="underline font-medium" href="tel:+46 72 399 15 15">+46 72 399 15 15 </a>
+                        <a className="underline font-semibold" href="tel:+46 72 399 15 15">+46 72 399 15 15 </a>
                     </div>
                     <div className="flex flex-row gap-4">
                         <Image src={PinIcon} alt="Pin icon"/>
-                        <a className="font-medium" href="">Nyköpingsvägen 52 | 611 50 NYKÖPING</a>
+                        <a className="font-semibold" href="">Nyköpingsvägen 52 | 611 50 NYKÖPING</a>
                     </div>
                 </div>   
             </div>
-            <div className="flex flex-col gap-8 font-medium">
+            <div className="flex flex-col gap-8 font-semibold">
                 <div className="flex flex-col gap-2">
                     <p>First and last name</p>
-                    <input className="rounded-[10px] border-pt-darkblue2 border-[1px] p-3 text-base" type="text" name="nameInput" id="" placeholder="First and last name" />
+                    <input className="rounded-[10px] border-pt-darkblue2 border-[1px] p-3 text-base font-medium" type="text" name="nameInput" id="" placeholder="First and last name" />
                 </div>
                 <div className="flex flex-col gap-2">
                     <p>Job title</p>
-                    <input className="rounded-[10px] border-pt-darkblue2 border-[1px] p-3" type="text" name="nameInput" id="" placeholder="Job title" />
+                    <input className="rounded-[10px] border-pt-darkblue2 border-[1px] p-3 font-medium" type="text" name="nameInput" id="" placeholder="Job title" />
                 </div>
                 <div className="flex flex-col gap-2">
                     <p>Email</p>
-                    <input className="rounded-[10px] border-pt-darkblue2 border-[1px] p-3" type="text" name="nameInput" id="" placeholder="ex. name@email.com" />
+                    <input className="rounded-[10px] border-pt-darkblue2 border-[1px] p-3 font-medium" type="text" name="nameInput" id="" placeholder="ex. name@email.com" />
                 </div>
-                <div className="flex flex-col gap-2">
+                <div className="flex flex-col gap-2 w-[70%]">
                     <p>Phone number</p>
-                    <input className="rounded-[10px] border-pt-darkblue2 border-[1px] p-3" type="text" name="nameInput" id="" placeholder="+46708123456" />
+                    <input className="rounded-[10px] border-pt-darkblue2 border-[1px] p-3 font-medium" type="text" name="nameInput" id="" placeholder="+46708123456" />
                 </div>
                 <div className="flex flex-col gap-2 ">
                     <p>Reason for contact</p>
@@ -73,11 +79,13 @@ const Contact = () => {
                 </div>
                 <div className="flex flex-col gap-2">
                     <p>Write us a message</p>
-                    <textarea name="" id="" placeholder="First and last name" className="rounded-[10px] border-pt-darkblue2 border-[1px] p-3  min-h-24 "></textarea>
+                    <textarea name="" id="" placeholder="Type your message..." className="rounded-[10px] border-pt-darkblue2 border-[1px] p-3 min-h-24 font-medium "></textarea>
                 </div>
                 <div className="flex align-middle gap-3 text-sm text-pt-darkblue2">
-                    <input className="size-[18px] my-auto rounded-[3px] border-pt-darkblue2" type="checkbox" name="checkbox" id="" /> 
-                    <span className="flex align-middle ">
+                <div  className=" h-[18px] w-[18px] border-pt-darkblue2 border-[1px] rounded-[3px] peer-checked:bg-pt-green">
+                <input className="left-[-1px] top-[-1px] h-[19px] w-[19px] rounded-[3px] opacity-0 cursor-pointer" type="checkbox" name="checkbox" id="" /> 
+                </div>
+                    <span className="flex align-middle font-medium">
                         <p >I accept the</p>
                         <a className="px-1 cursor-pointer underline" href="">
                             <p>Terms</p>
@@ -94,6 +102,16 @@ const Contact = () => {
 export default Contact
 
 /*
+
+<input className="peer  left-[-1px] top-[-1px] h-[19px] w-[19px] rounded-[3px]  cursor-pointer" type="checkbox" name="checkbox" id="" /> 
+                    <span  className=" h-[28px] w-[28px] border-pt-darkblue2 border-[1px] rounded-[3px]peer-checked:border-3">
+                    </span>
+
+<span className="relative z-20 h-[18px] w-[18px] border-pt-darkblue2 bg-pt-green rounded-[3px]">
+                    <input className="absolute hidden z-10 top-0 left-0 h-[18px] w-[18px]" type="checkbox" name="checkbox" id="" /> 
+                    </span>
+
+size-[18px] my-auto rounded border-white hover:border-blue
 
   <div className="mt-[80px] font-mono">
                 <div className="">
