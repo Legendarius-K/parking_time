2c320e8f2fda6121700776807fdf3bfa28cbb85f
Darius K <darius.kaya@gmail.com>
2024-06-05 10:47:36 +0200

    Desktop button in desktop view

--- src/components/How/index.tsx
+++ src/components/How/index.tsx
@@ -1,18 +1,23 @@
-import arrow from '../../../public/Vectorarrow.png'
+'use client'
+
+import arrow from '../../../public/Vectorarrow.png';
 import Image from 'next/image';
 import ButtonMobile from '../ButtonMobile';
 import HowBox from '../HowBox';
-import ptPen from '../../../public/pen.svg'
-import ptPin from '../../../public/parkpin.svg'
-import ptClock from '../../../public/clock.svg'
+import ptPen from '../../../public/pen.svg';
+import ptPin from '../../../public/parkpin.svg';
+import ptClock from '../../../public/clock.svg';
+import useWindowSize from '@/hooks/useWindowSize';
+import Button from '../Button'; // Assuming you have a Button component
 
 const How = () => {
+    const { width } = useWindowSize();
+
     return (
         <div className='bg-pt-background flex justify-center'>
-            <section className="max-w-[1350px] py-16 md:px-10 flex flex-col items-center">
+            <section className="max-w-[1350px] py-16 md:px-10 flex flex-col items-center w-[70%] md:w-full ">
                 <h2 className="text-center text-3xl md:text-4xl m-6">Hur det fungerar</h2>
                 <div className="flex flex-col md:flex-row m-8 justify-between items-center">
-
                     <HowBox srcIcon={ptPen} altIcon='A Pen' number='01' heading='Fyll i registreringsnummer' text='Öppna appen, registrera dig med ditt registreringsnummer.' />
 
                     <div className='flex justify-center rotate-90 m-4 md:rotate-0'>
@@ -26,12 +31,15 @@ const How = () => {
                     </div>
 
                     <HowBox srcIcon={ptClock} altIcon='A Clock' number='03' heading='Starta din digitala p-skiva' text='När du har hittat din parkeringszon kan du se över parkeringens regler, starta parkering och stänga appen.' />
-
                 </div>
-                <ButtonMobile targetBlank={true} route={'https://apps.apple.com/se/app/parking-time/id1611019108'} btnText='Ladda ner appen' colors='bg-black text-white' />
+                {width && width > 960 ? (
+                    <Button targetBlank={true} route={'https://apps.apple.com/se/app/parking-time/id1611019108'} btnText='Ladda ner appen' colors='bg-black text-white' />
+                ) : (
+                    <ButtonMobile targetBlank={true} route={'https://apps.apple.com/se/app/parking-time/id1611019108'} btnText='Ladda ner appen' colors='bg-black text-white' />
+                )}
             </section>
         </div>
-    )
+    );
 };
 
-export default How
+export default How;
