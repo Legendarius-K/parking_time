e13daf1ac3b1b0a760e77d83f0826473a0dadadd
Africandeluxe <miski.mohamedahmed@gmail.com>
2024-06-06 20:21:19 +0200

    Changes made

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Hero from "@/components/Home-Hero";
 import SmartParkingSolution from "@/components/SmartParkingSolution"
 
+
 const Home = () => {
     return (
         <main className="">
