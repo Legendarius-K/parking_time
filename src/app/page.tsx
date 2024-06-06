c03df4f1bbb8e00d67b0ddb62992c5c7e2557215
Lisette Svan <svanlisette@gmail.com>
2024-06-06 23:00:53 +0200

    imported why into page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import How from "@/components/How";
 import Image from "next/image";
 import Hero from "@/components/Home-Hero";
 import SmartParkingSolution from "@/components/SmartParkingSolution"
+import Why from "@/components/Home/Why";
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <Hero />
             <SmartParkingSolution />
             <How />
+            <Why />
         </main>
     );
 }
