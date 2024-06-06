573afdf6d9f5469b9f81feaf2c0781a3ea446657
Africandeluxe <miski.mohamedahmed@gmail.com>
2024-06-06 15:36:41 +0200

    Fixed li on hero for responsive

--- src/components/Home-Hero/index.tsx
+++ src/components/Home-Hero/index.tsx
@@ -19,15 +19,15 @@ const Homehero: React.FC = () => {
         <Button targetBlank={true} route={'https://apps.apple.com/se/app/parking-time/id1611019108'} btnText="Download app" colors="bg-custom-black text-white" />
         </div>
       </div>
-      <div className="absolute bottom-0 left-0 w-full bg-pt-green p-4 text-center text-white bg-opacity-80">
-        <ul className="flex md:flex md:justify-around md: p-4 text-[16px] leading-[32px] text-xs md:text-sm lg:text-base list-disc list-inside p-0 m-0">
-          <li className="flex justify-start items-center h-12 md:h-auto font-roboto font-medium leading-8">Easy to use</li>
-          <li className="flex justify-start items-center h-12 md:h-auto font-roboto font-medium leading-8">Free to download</li>
-          <li className="flex justify-start items-center h-12 md:h-auto font-roboto font-medium leading-8">Avoid fines</li>
-          <li className="flex justify-start items-center h-12 md:h-auto font-roboto font-medium leading-8text-left">Reduce Stress</li>
+      <div className="absolute bottom-0 left-0 w-full bg-pt-green px-8 py-3.5 text-center text-white bg-opacity-80">
+        <ul className="grid grid-cols-2 md:flex md:justify-around text-xs md:text-sm lg:text-base list-disc list-inside gap-4">
+          <li className="font-roboto font-medium leading-8 text-left px-4">Easy to use</li>
+          <li className="font-roboto font-medium leading-8 text-left px-4">Free to download</li>
+          <li className="font-roboto font-medium leading-8 text-left px-4">Avoid fines</li>
+          <li className="font-roboto font-medium leading-8 text-left px-4">Reduce Stress</li>
         </ul>
       </div>
-    </div>
+  </div>
   );
 }
 
