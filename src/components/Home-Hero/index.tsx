d09d2a7f11772455b27d84a5a3c94b1d07f1359b
Africandeluxe <miski.mohamedahmed@gmail.com>
2024-06-06 19:20:37 +0200

    Changes made to syle

--- src/components/Home-Hero/index.tsx
+++ src/components/Home-Hero/index.tsx
@@ -6,7 +6,7 @@ const Homehero: React.FC = () => {
   return (
     <div className="relative bg-cover bg-center h-screen" style={{ backgroundImage: 'url(/hero-bg.png)' }}>
       <div className="absolute inset-0 bg-black bg-opacity-40"></div>
-      <div className="relative flex flex-col items-start justify-center h-full text-white px-4 md:px-8 lg:px-16" style={{ marginLeft: '40px' }}>
+      <div className="relative flex flex-col items-start justify-center h-full text-white md:px-8 lg:px-16" style={{ marginLeft: '40px' }}>
         <h1 className="text-[42px] text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold mb-4 leading-tight w-full md:w-3/4 lg:w-2/3 xl:w-1/2" style={{ opacity: 1 }}>
           What time is it? <br /> Parking Time!
         </h1>
@@ -16,7 +16,7 @@ const Homehero: React.FC = () => {
         </p>
       <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4">
         <Link href="/contact">
-          <button className="bg-white text-custom-black  w-full max-w-[400px] flex justify-center px-8 py-3 rounded-full font-light transition" style={{ opacity: 1 }}>Contact us</button>
+          <button className="bg-white text-custom-black  w-full max-w-[400px] flex justify-center px-12 py-3 rounded-full font-light transition" style={{ opacity: 1 }}>Contact us</button>
         </Link>
         <Link href="https://apps.apple.com/se/app/parking-time/id1611019108" legacyBehavior>
           <a target="_blank" rel="noopener noreferrer">
