b3f089a0d3edfed39fe72ffc051959eed5015d59
Darius K <darius.kaya@gmail.com>
2024-06-12 11:10:27 +0200

    google analytics attempt 1

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -5,12 +5,13 @@ import Footer from "@/components/Footer";
 import { Roboto } from "next/font/google";
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
+import Script from "next/script";
 
 const roboto = Roboto({ subsets: ['latin'], weight: '400' })
 
 // const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = { 
+export const metadata: Metadata = {
     title: "Parking Time",
     description: "What time is it? Parking Time!",
 };
@@ -32,6 +33,21 @@ export default async function RootLayout({
     return (
         <html lang={locale} className="h-screen">
             <head>
+                <Script
+                    strategy="lazyOnload"
+                    src={`https://www.googletagmanager.com/gtag/js?id=G-CZYWDJ87Z0`}
+                />
+
+                <Script id="ga-script" strategy="lazyOnload">
+                    {`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'G-CZYWDJ87Z0', {
+                        page_path: window.location.pathname,
+                        });
+                    `}
+                </Script>
                 <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
             </head>
             <body className={`${'font-sans font-thin flex flex-col min-h-full box-border'}`}>
