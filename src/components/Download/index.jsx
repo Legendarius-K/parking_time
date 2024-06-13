821fc3b77315a3b9e7378a4ffb5256300d62b9e7
Lisette Svan <svanlisette@gmail.com>
2024-06-13 18:26:17 +0200

    made changes from CR

--- src/components/Download/index.jsx
+++ src/components/Download/index.jsx
@@ -4,13 +4,13 @@ const Download = () => {
     const t = useTranslations('download')
         return (
             <>
-                <div className="flex flex-col md:flex-row justify-center items-center mt-[3%] content-normal">
-                    <div className="order-1 md:order-2 mb-8 md:mb-[8%] flex justify-center items-center md:w-auto">
+                <div className="flex flex-col md:flex-row mb-[8%] md:mr-[1%] items-center justify-center mt-[8%] content-normal">
+                    <div className="order-1 md:order-2 mb-[2%] mb:mb-0 flex justify-center items-center md:w-auto">
                         <img src="/iphone.svg" alt="hand with iphone" className="w-full" />
                     </div>
                     <div className="order-2 md:order-1 flex flex-col max-w-lg w-full md:w-auto">
                         <h1 className="text-5xl mt-3 ml-[5%]">{t("title")}</h1>
-                        <p className="font-nunito font-thin w-full md:w-1000%] leading-loose mt-5 p-[5%]">
+                        <p className="font-nunito font-thin w-full md:w-1000%] leading-loose p-[5%]">
                             {t("paragraf")}
                         </p>
                         <div className="flex mb-[7%] ml-[4%] mb:[2%] md:mb-0">
