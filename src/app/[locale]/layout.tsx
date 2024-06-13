f121f2170e0be1d470f3f125c99ef6cba359c929
Darius K <darius.kaya@gmail.com>
2024-06-13 11:28:45 +0200

    try new package

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -5,8 +5,7 @@ import Footer from "@/components/Footer";
 import { Roboto } from "next/font/google";
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
-import Script from "next/script";
-import Analytics from "@/components/Analytics";
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 const roboto = Roboto({ subsets: ['latin'], weight: '400' })
 
@@ -34,48 +33,11 @@ export default async function RootLayout({
     return (
         <html lang={locale} className="h-screen">
             <head>
-
-
-                {/* <script
-                    async
-                    src={"https://www.googletagmanager.com/gtag/js?id=G-G-CZYWDJ87Z0"}
-                />
-
-                <script
-                    dangerouslySetInnerHTML={{
-                        __html: `
-                        window.dataLayer = window.dataLayer || [];
-                        function gtag(){dataLayer.push(arguments);} 
-                        gtag('js', new Date());
-                        gtag('config', 'G-CZYWDJ87Z0', {
-                            page_path: window.location.pathname,
-                        });
-                    `,
-                    }}
-
-                /> */}
-
-
-                {/* <Script
-                    strategy="lazyOnload"
-                    src={`https://www.googletagmanager.com/gtag/js?id=G-CZYWDJ87Z0`}
-                />
-
-                <Script id="ga-script" strategy="lazyOnload">
-                    {`
-                        window.dataLayer = window.dataLayer || [];
-                        function gtag(){dataLayer.push(arguments);} 
-                        gtag('js', new Date());
-                        gtag('config', 'G-CZYWDJ87Z0', {
-                        page_path: window.location.pathname,
-                        });
-                    `}
-                </Script> */}
                 <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
             </head>
             <body className={`${'font-sans font-thin flex flex-col min-h-full box-border'}`}>
-                <Analytics />
                 <NextIntlClientProvider messages={messages}>
+                    <GoogleAnalytics trackPageViews />
                     <Header />
                     <section className="grow">
                         {children}
