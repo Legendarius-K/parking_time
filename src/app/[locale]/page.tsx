0dab28fda13f8452e0a6ba5dbf3886ec46215347
Darius K <darius.kaya@gmail.com>
2024-06-13 14:21:10 +0200

    done

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,26 +1,27 @@
 import How from "@/components/How";
-import Image from "next/image";
 import Hero from "@/components/Home-Hero";
-import SmartParkingSolution from "@/components/SmartParkingSolution"
-import { useTranslations } from "next-intl";
+import SmartParkingSolution from "@/components/SmartParkingSolution";
 import Why from "@/components/Why";
 import CarDriving from "@/components/CarDriving";
 import TrustedBy from "@/components/TrustedBy";
-
-
+import Faq from "@/components/Faq";
 
 const Home = () => {
-    const t = useTranslations('navigation')
     return (
         <main className="overflow-hidden">
             <Hero />
             <SmartParkingSolution />
             <CarDriving />
             <How />
-            <Why />
+            <div id="why-component">
+                <Why />
+            </div>
             <TrustedBy />
+            <div id="faq-component">
+                <Faq />
+            </div>
         </main>
     );
-}
+};
 
 export default Home;
---
+++ src/components/Faq/index.tsx
@@ -0,0 +1,11 @@
+
+
+const Faq = () => {
+    return (
+        <>
+            <div>faq</div>
+        </>
+    )
+};
+
+export default Faq
