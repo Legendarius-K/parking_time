0f218950970f187a23d2c8c1b1fd031e1b4e325b
Darius K <darius.kaya@gmail.com>
2024-06-13 12:01:34 +0200

    back to next.js-ga AGAIN

--- src/components/Analytics/index.tsx
+++ src/components/Analytics/index.tsx
@@ -1,55 +1,55 @@
-// 'use client';
-
-// import { GoogleAnalytics } from 'nextjs-google-analytics';
-// import { useEffect } from 'react';
-
-// const GoogleAnalyticsClient = () => {
-//     useEffect(() => {
-//         // Optionally, you can add any additional setup or logging here
-//         console.log('Google Analytics initialized');
-//     }, []);
-
-//     return <GoogleAnalytics trackPageViews />;
-// };
-
-// export default GoogleAnalyticsClient;
-
-
-
-
-
-
-
 'use client';
 
-import Script from 'next/script';
-import React from 'react';
+import { GoogleAnalytics } from 'nextjs-google-analytics';
+import { useEffect } from 'react';
 
-const GoogleAnalytics = () => {
-    const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
+const GoogleAnalyticsClient = () => {
+    useEffect(() => {
+        // Optionally, you can add any additional setup or logging here
+        console.log('Google Analytics initialized');
+    }, []);
 
-    return (
-        <>
-            <Script
-                strategy="afterInteractive"
-                src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
-            />
-            <Script
-                id="google-analytics"
-                strategy="afterInteractive"
-                dangerouslySetInnerHTML={{
-                    __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_MEASUREMENT_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-                }}
-            />
-        </>
-    );
+    return <GoogleAnalytics trackPageViews />;
 };
 
-export default GoogleAnalytics;
+export default GoogleAnalyticsClient;
+
+
+
+
+
+
+
+// 'use client';
+
+// import Script from 'next/script';
+// import React from 'react';
+
+// const GoogleAnalytics = () => {
+//     const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
+
+//     return (
+//         <>
+//             <Script
+//                 strategy="afterInteractive"
+//                 src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+//             />
+//             <Script
+//                 id="google-analytics"
+//                 strategy="afterInteractive"
+//                 dangerouslySetInnerHTML={{
+//                     __html: `
+//             window.dataLayer = window.dataLayer || [];
+//             function gtag(){dataLayer.push(arguments);}
+//             gtag('js', new Date());
+//             gtag('config', '${GA_MEASUREMENT_ID}', {
+//               page_path: window.location.pathname,
+//             });
+//           `,
+//                 }}
+//             />
+//         </>
+//     );
+// };
+
+// export default GoogleAnalytics;
