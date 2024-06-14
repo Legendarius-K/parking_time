0aa2c20c9eee6e1f1b1bdb2c78bfd2ca6adc0198
Lisette Svan <svanlisette@gmail.com>
2024-06-14 08:18:19 +0200

    changed spacing

--- src/components/KnowMore/index.jsx
+++ src/components/KnowMore/index.jsx
@@ -7,7 +7,7 @@ const KnowMore = () => {
     const t = useTranslations("knowMore");
     return (
         <div className="flex justify-center items-center h-[50vh]">
-            <div className="text-center leading-10 p-4 md:p-0">
+            <div className="text-center leading-7 p-4 md:p-0">
                 <h1 className="text-5xl font-semibold mt-8 md:mt-12">{t("title")}</h1>
                 <p className="font-nunito font-thin max-w-md mx-auto mt-4 md:mt-6">{t("paragraf")}</p>
                 <div className="flex justify-center mt-6 md:mt-8">
