6ba181704db1e424adf923751a22863903337a3f
timdeckel <timdeckel@gmail.com>
2024-06-14 08:54:37 +0200

    made an oopsie

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,8 +1,13 @@
 import { createClient } from 'contentful';
 
+// const client = createClient({
+//     space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID as string,
+//     accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_KEY as string,
+// })
+
 const client = createClient({
-    space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID as string,
-    accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_KEY as string,
+  space: 'lfi9rred70pl',
+  accessToken: '5f7nwoM_rpxKGtW2lx1qRSca3QvJ3HxfrcqAosyAV4A',
 })
 
 export const fetchTestimonials = async () => {
