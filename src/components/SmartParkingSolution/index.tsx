22daaf4ceb94c26ec0c0fee6d20777d0a2b0a9e0
Darius K <darius.kaya@gmail.com>
2024-06-16 12:56:42 +0200

    scroll animation on desktop done. Mobile = static img

--- src/components/SmartParkingSolution/index.tsx
+++ src/components/SmartParkingSolution/index.tsx
@@ -26,7 +26,7 @@ const SmartParkingSolution: React.FC = () => {
     const y1 = useTransform(scrollYProgress, [0, 1], [170, -170]);
     const y2 = useTransform(scrollYProgress, [0, 1], [70, -70]);
     const y3 = useTransform(scrollYProgress, [0, 1], [90, -90]);
-    const y4 = useTransform(scrollYProgress, [0, 1], [50, -50]);
+    const y4 = useTransform(scrollYProgress, [0, 1], [50, -10]);
     // const opacity = useTransform(scrollYProgress, [0.25, 0.5, 0.75], [0, 1, 0]);
 
     const t = useTranslations('smart-parking')
@@ -34,37 +34,43 @@ const SmartParkingSolution: React.FC = () => {
     return (
         <div className='flex justify-center'>
             <div className="flex flex-col justify-center md:flex-row bg-white p-8 md:p-16 space-y-8 md:space-y-0 md:space-x-8 max-w-[1400px]">
-                <div className="relative  lg:mr-[70px] w-fit h-fit flex justify-center md:justify-center">
-                    <div className='relative w-4/5'>
-                        <Image className='' src={phoneBg} alt='background' style={{ width: "100%", height: "auto" }} />
-                        {/* <div className='absolute bottom-[29px] right-[170px] w-[310px]'>
+                <div className='flex justify-center items-center'>
+                    <div className="hidden lg:flex relative lg:mr-[70px] w-fit h-fit justify-center md:justify-center">
+                        <div className='relative w-4/5'>
+                            <Image className='' src={phoneBg} alt='background' style={{ width: "100%", height: "auto" }} />
+                            {/* <div className='absolute bottom-[29px] right-[170px] w-[310px]'>
                             <Image src={phone} alt='phone' style={{ width: "100%", height: "auto" }} />
                         </div> */}
+                        </div>
+                        <motion.div
+                            ref={targetRef}
+                            style={{ y: y1 }}
+                            className='w-[100px] md:w-[140px] absolute -left-4 top-[60px] '>
+                            <Image src={reg} alt='reg nr' style={{ width: "100%", height: "auto" }} />
+                        </motion.div>
+                        <motion.div
+                            ref={targetRef}
+                            style={{ y: y2 }}
+                            className='w-[100px] md:w-[140px] absolute -left-8 bottom-12'>
+                            <Image src={rules} alt='rules sign' style={{ width: "100%", height: "auto" }} />
+                        </motion.div>
+                        <motion.div
+                            ref={targetRef}
+                            style={{ y: y3 }}
+                            className='w-[100px] md:w-[150px] absolute -right-8 md:-right-12 top-14'>
+                            <Image src={zones} alt='parking zones' style={{ width: "100%", height: "auto" }} />
+                        </motion.div>
+                        <motion.div
+                            ref={targetRef}
+                            style={{ y: y4 }}
+                            className='w-[100px] md:w-[140px] absolute -right-8 bottom-16'>
+                            <Image src={note} alt='notification' style={{ width: "100%", height: "auto" }} />
+                        </motion.div>
+                        
                     </div>
-                    <motion.div 
-                        ref={targetRef}
-                        style={{ y: y1 }}
-                        className='w-[100px] md:w-[140px] absolute -left-4 top-[60px] '>
-                        <Image src={reg} alt='reg nr' style={{ width: "100%", height: "auto" }} />
-                    </motion.div>
-                    <motion.div 
-                        ref={targetRef}
-                        style={{ y: y2 }}
-                        className='w-[100px] md:w-[140px] absolute -left-8 bottom-12'>
-                        <Image src={rules} alt='rules sign' style={{ width: "100%", height: "auto" }} />
-                    </motion.div>
-                    <motion.div 
-                        ref={targetRef}
-                        style={{ y: y3 }}
-                        className='w-[100px] md:w-[140px] absolute -right-8 md:-right-6 top-14'>
-                        <Image src={zones} alt='parking zones' style={{ width: "100%", height: "auto" }} />
-                    </motion.div>
-                    <motion.div
-                        ref={targetRef}
-                        style={{ y: y4 }}
-                        className='w-[100px] md:w-[140px] absolute -right-6 bottom-16'>
-                        <Image src={note} alt='notification' style={{ width: "100%", height: "auto" }} />
-                    </motion.div>
+                </div>
+                <div className="w-full md:w-1/2 flex lg:hidden justify-center md:justify-center">
+                    <img src="/Smart-Parking-Solution2.png" alt="Smart Parking Solution" className="w-full md:w-auto object-contain" />
                 </div>
                 <div className="w-full md:w-1/2 flex flex-col justify-center space-y-4 md:space-y-6">
                     <h2 className="text-[38px] leading-[43.2px]text-2xl md:text-4xl font-bold pt-darkblue1-900">{t('headline')}</h2>
