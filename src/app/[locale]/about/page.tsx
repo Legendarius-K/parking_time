b2f0d23ea5689c9b6d2a57a5a0074593e2c63fa0
Lisette Svan <svanlisette@gmail.com>
2024-06-17 16:04:41 +0200

    done for now, waiting on CR

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import AboutHero from '@/components/AboutHero';
 import KnowMore from '../../../components/KnowMore';
+import OurVisionStory from '@/components/OurVisionStory';
 
 const About = () => {
     return (
         <div>
             <AboutHero />
+            <OurVisionStory />
             <KnowMore />
         </div>
     );
---
+++ src/components/OurVisionStory/index.tsx
@@ -0,0 +1,40 @@
+import { Link } from '@/navigation';
+import { useTranslations } from 'next-intl';
+import React from 'react';
+import Button from "../Button";
+
+const OurVisionStory = () => {
+    const t = useTranslations("ourStory");
+    return (
+        <>
+        <div className="relative flex flex-col lg:flex-row ml-[10%] mr-[8%] justify-center">
+            <div className="order-2 lg:order-1 flex flex-col w-full lg:w-[55%]">
+                <div>
+                    <h1 className="text-3xl lg:text-3xl mb-6 lg:mb-8 mt-[25%]">{t('titleOne')}</h1>
+                    <p className="w-[90%] mb-6 lg:mb-10 text-lg lg:text-base">{t('paragrafOne')}</p>
+                    <p className="w-[85%] text-lg lg:text-base">{t('titleTwo')}</p>
+                </div>
+                <div>
+                    <h1 className="text-3xl lg:text-3xl mt-10 mb-6 lg:mb-8">{t('titleTwo')}</h1>
+                    <p className="w-[100%] mb-6 lg:mb-2 text-lg lg:text-base">{t('paragrafThree')}</p>
+                    <Link href="/contact">
+                        <button className="bg-custom-black text-white w-full lg:w-auto lg:max-w-xs mx-auto px-8 py-3 rounded-full font-light transition hover:bg-gray-300 mt-10 lg:mt-10">
+                            {t('button')}
+                        </button>
+                    </Link>
+                </div>
+            </div>
+            <div className="order-1 lg:order-2 w-full lg:w-[100%] mb-10 mt-[7%] lg:block hidden ml-[10%]">
+                <img
+                    src="/story.svg"
+                    alt="our story picture"
+                    className="max-w-full w-full h-auto object-cover transition-opacity duration-300 lg:max-w-none lg:w-auto"
+                    style={{ maxWidth: "100%" }}
+                />
+            </div>
+        </div>
+    </>
+    );
+}
+
+export default OurVisionStory;
