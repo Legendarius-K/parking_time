e0a218789208513345f6c9fd150c42753b250e8f
Darius K <darius.kaya@gmail.com>
2024-06-18 11:51:37 +0200

    done

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import AboutHero from '@/components/AboutHero';
 import KnowMore from '../../../components/KnowMore';
+import MeetTheTeam from '@/components/MeetTheTeam';
 
 const About = () => {
     return (
         <div>
             <AboutHero />
+            <MeetTheTeam />
             <KnowMore />
         </div>
     );
