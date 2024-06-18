34b07ca9aee45883b12810a7f0da9405939e4fcc
Darius K <darius.kaya@gmail.com>
2024-06-18 12:56:41 +0200

    size bug vercel test - w-full

--- src/components/SmartParkingSolution/index.tsx
+++ src/components/SmartParkingSolution/index.tsx
@@ -11,7 +11,6 @@ import phoneBg from '../../../public/smart-phone-bg.png'
 import Image from 'next/image';
 import { motion, useScroll, useTransform } from 'framer-motion'
 
-
 const SmartParkingSolution: React.FC = () => {
 
     const targetRef = useRef(null);
@@ -29,39 +28,39 @@ const SmartParkingSolution: React.FC = () => {
     const x3 = useTransform(scrollYProgress, [0, 1], [-10, 10]);
     const x4 = useTransform(scrollYProgress, [0, 1], [10, -10]);
 
-    const t = useTranslations('smart-parking') 
+    const t = useTranslations('smart-parking');
 
     return (
         <div className='flex justify-center'>
             <div className="flex flex-col justify-center md:flex-row bg-white p-8 md:p-16 space-y-8 md:space-y-0 md:space-x-8 max-w-[1400px]">
                 <div className='flex justify-center items-center'>
                     <div className="hidden lg:flex relative lg:mr-[70px] w-fit h-fit justify-center md:justify-center">
-                        <div className='relative min-w-[80%]'>
-                            <Image className='' layout="responsive" src={phoneBg} alt='background' style={{ width: "100%", height: "auto" }} />
+                        <div className='relative w-full'>
+                            <Image src={phoneBg} alt='background' layout="responsive" style={{ width: "100%", height: "auto" }} />
                         </div>
                         <motion.div
                             ref={targetRef}
                             style={{ y: y1, x: x1 }}
-                            className='w-[100px] md:w-[140px] absolute -left-4 top-[60px] '>
-                            <Image src={reg} alt='reg nr' style={{ width: "100%", height: "auto" }} />
+                            className='w-[100px] md:w-[140px] absolute -left-4 top-[60px]'>
+                            <Image src={reg} alt='reg nr' layout="responsive" style={{ width: "100%", height: "auto" }} />
                         </motion.div>
                         <motion.div
                             ref={targetRef}
                             style={{ y: y2, x: x2 }}
                             className='w-[100px] md:w-[140px] absolute -left-8 bottom-12'>
-                            <Image src={rules} alt='rules sign' style={{ width: "100%", height: "auto" }} />
+                            <Image src={rules} alt='rules sign' layout="responsive" style={{ width: "100%", height: "auto" }} />
                         </motion.div>
                         <motion.div
                             ref={targetRef}
                             style={{ y: y3, x: x3 }}
                             className='w-[100px] md:w-[150px] absolute -right-8 md:-right-12 top-14'>
-                            <Image src={zones} alt='parking zones' style={{ width: "100%", height: "auto" }} />
+                            <Image src={zones} alt='parking zones' layout="responsive" style={{ width: "100%", height: "auto" }} />
                         </motion.div>
                         <motion.div
                             ref={targetRef}
                             style={{ y: y4, x: x4 }}
                             className='w-[100px] md:w-[140px] absolute -right-8 bottom-20'>
-                            <Image src={note} alt='notification' style={{ width: "100%", height: "auto" }} />
+                            <Image src={note} alt='notification' layout="responsive" style={{ width: "100%", height: "auto" }} />
                         </motion.div>
                     </div>
                 </div>
@@ -69,11 +68,11 @@ const SmartParkingSolution: React.FC = () => {
                     <img src="/Smart-Parking-Solution3.png" alt="Smart Parking Solution" className="w-full md:w-auto object-contain" />
                 </div>
                 <div className="w-full md:w-1/2 flex flex-col justify-center space-y-4 md:space-y-6">
-                    <h2 className="text-[38px] leading-[43.2px]text-2xl md:text-4xl font-bold pt-darkblue1-900">{t('headline')}</h2>
-                    <p className="text-[16px] leading-[24px]text-sm md:text-base pt-darkblue1-700">
+                    <h2 className="text-[38px] leading-[43.2px] text-2xl md:text-4xl font-bold pt-darkblue1-900">{t('headline')}</h2>
+                    <p className="text-[16px] leading-[24px] text-sm md:text-base pt-darkblue1-700">
                         {t('text1')}
                     </p>
-                    <ul className="space-y-2 md:space-y-">
+                    <ul className="space-y-2 md:space-y-4">
                         <li className="flex items-center space-x-2">
                             <span className="text-pt-red font-semibold">›</span>
                             <span className="text-[16px] leading-[24px] text-sm md:text-base pt-darkblue1 font-semibold py-0.5 md:py-0">{t('bullet1')}</span>
