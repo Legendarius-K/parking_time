2cb5505543302e04577fd2267ef6bf761ea22d78
Lisette Svan <svanlisette@gmail.com>
2024-06-19 12:03:10 +0200

    made a few more changes

--- src/components/OurVisionStory/index.tsx
+++ src/components/OurVisionStory/index.tsx
@@ -7,16 +7,16 @@ const OurVisionStory = () => {
     const t = useTranslations("ourStory");
     return (
         <>
-        <div className="relative flex flex-col lg:flex-row ml-[10%] mr-[8%] justify-center">
-            <div className="order-2 lg:order-1 flex flex-col w-full lg:w-[55%]">
+        <div className="relative flex flex-col lg:flex-row ml-[13%] mr-[8%] justify-center">
+            <div className="order-2 lg:order-1 flex flex-col w-full lg:w-[55%] mr-[3%]">
                 <div>
                     <h1 className="text-3xl lg:text-3xl mb-6 lg:mb-8 mt-[35%]">{t('titleOne')}</h1>
-                    <p className="font-nunito mb-6 lg:mb-8 text-lg lg:text-base">{t('paragrafOne')}</p>
-                    <p className="font-nunito text-lg lg:text-base">{t('paragrafTwo')}</p>
+                    <p className=" font-nunito mb-6 lg:mb-8 text-lg lg:text-base">{t('paragrafOne')}</p>
+                    <p className="  font-nunito text-lg lg:text-base">{t('paragrafTwo')}</p>
                 </div>
                 <div>
                     <h1 className="text-3xl lg:text-3xl mt-8 mb-6 lg:mb-8">{t('titleTwo')}</h1>
-                    <p className="font-nunito mb-6 lg:mb-2 text-lg lg:text-base">{t('paragrafThree')}</p>
+                    <p className=" font-nunito mb-6 lg:mb-2 text-lg lg:text-base">{t('paragrafThree')}</p>
                     <Link href="/contact">
                         <button className="bg-custom-black text-white w-full lg:w-auto lg:max-w-xs mx-auto px-8 py-3 rounded-full font-light transition hover:bg-gray-300 mt-10 lg:mt-8">
                             {t('button')}
@@ -24,12 +24,12 @@ const OurVisionStory = () => {
                     </Link>
                 </div>
             </div>
-            <div className="order-1 lg:order-2 w-full lg:w-[100%] mb-10 mt-[7%] lg:block hidden ml-[10%]">
+            <div className="order-1 justify-center items-center lg:order-2 w-full lg:w-[100%] mb-10 mt-[8%] lg:block hidden ">
                 <img
                     src="/story.svg"
                     alt="our story picture"
                     className="max-w-full w-full h-auto object-cover transition-opacity duration-300 lg:max-w-none lg:w-auto"
-                    style={{ maxWidth: "100%" }}
+                    style={{ maxWidth: "115%" }}
                 />
             </div>
         </div>
