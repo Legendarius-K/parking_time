9b1051658ccca658218b2486bd217a2ad5c40928
Darius K <darius.kaya@gmail.com>
2024-06-20 21:08:04 +0200

    adjusted responsiveness

--- src/components/OurVisionStory/index.tsx
+++ src/components/OurVisionStory/index.tsx
@@ -2,15 +2,17 @@ import { Link } from '@/navigation';
 import { useTranslations } from 'next-intl';
 import React from 'react';
 import Button from "../Button";
+import Image from 'next/image';
+import story from '../../../public/story.svg'
 
 const OurVisionStory = () => {
     const t = useTranslations("ourStory");
     return (
-        <>
-            <div className="relative flex flex-col lg:flex-row lg-190:flex-col ml-[13%] mr-[8%] justify-center">
-                <div className="order-2 lg:order-1 flex flex-col w-full lg:w-[55%] mr-[3%]">
+        <div className='flex justify-center'>
+            <div className="relative flex flex-col lg:flex-row items-center justify-center px-10 mt-20 max-w-[1300px]">
+                <div className="order-2 lg:order-1 flex flex-col w-full lg:w-[65%] lg:mr-[3%]">
                     <div>
-                        <h1 className="text-3xl lg:text-3xl mb-6 lg:mb-8 mt-[35%]">{t('titleOne')}</h1>
+                        <h1 className="text-3xl lg:text-3xl mb-6 lg:mb-8 ">{t('titleOne')}</h1>
                         <p className=" font-nunito mb-6 lg:mb-8 text-lg lg:text-base">{t('paragrafOne')}</p>
                         <p className="font-nunito text-lg lg:text-base">{t('paragrafTwo')}</p>
                     </div>
@@ -24,16 +26,19 @@ const OurVisionStory = () => {
                         </Link>
                     </div>
                 </div>
-                <div className="order-1 justify-center items-center lg:order-2 w-full lg:w-[100%] mb-10 mt-[8%] lg:block hidden">
+                <div className='order-1 h-fit w-full mb-14 lg:mb-0 max-w-[800px] hidden sm:block'>
+                    <Image src={story} alt='Story' style={{ width: "100%", height: "auto" }} />
+                </div>
+                {/* <div className="order-1 md:flex md:justify-center md:items-center lg:order-2 w-full lg:w-[100%] mb-10 mt-[8%] lg:block hidden">
                     <img
                         src="/story.svg"
                         alt="our story picture"
                         className="max-w-full w-full h-auto object-cover transition-opacity duration-300 lg:max-w-none lg:w-auto"
                         style={{ maxWidth: "115%" }}
                     />
-                </div>
+                </div> */}
             </div>
-        </>
+        </div>
     );
 };
 
