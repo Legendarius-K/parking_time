3089e400977b663d927bf2434c26f24caa256cb0
timdeckel <timdeckel@gmail.com>
2024-06-20 16:47:14 +0200

    single page news article now has access to correct article based on url slug

--- src/app/[locale]/news/[article]/page.tsx
+++ src/app/[locale]/news/[article]/page.tsx
@@ -1,19 +1,52 @@
-// pages/article.js (or pages/article/index.js if using directory structure)
-import { useRouter } from 'next/navigation';
+'use client';
+import React, { useState, useEffect } from 'react';
+import { fetchNewsArticle } from '@/utils/functions';
 
+interface ArticleContent {
+  title: string;
+  paragraph: string;
+  date: string;
+  image: string;
+  language: string;
+}
 
+const article = ({ params }) => {
+  const slug  = params.article;
+  const [article, setArticle] = useState<ArticleContent>();
+  const [articleExists, setarticleExists] = useState(false);
 
-
-const article = ({params}) => {
-
-  console.log("params i article " + {params})
-
-  // Optional: Replace this with actual data fetching logic based on 'article'
-  // For example, fetching article content from an API based on 'article' parameter
-
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const articles = await fetchNewsArticle();
+                const filteredArticle = articles.find((article) => article.fields.slug === slug);
+                if (filteredArticle) {
+                  const formattedArticle: any = {
+                    title: filteredArticle.fields.shortTitle,
+                    paragraph: filteredArticle.fields.shortText,
+                    date: filteredArticle.fields.date,
+                    image: filteredArticle.fields.imageUrl,
+                    language: filteredArticle.fields.language,
+                  };
+                  setArticle(formattedArticle);
+                  setarticleExists(true)
+                }
+            } catch (error) {
+                console.log("error något gick fel")
+            }
+        }
+        fetchData();
+    }, [slug]);
+    if (!articleExists) {
+      return <div>Article does not exist</div>
+    }
+    if (!article) {
+      return <div>Loading...</div>;
+    }
+   
   return (
     <div>
-      <h1>Article: {params.article}</h1>
+      <h1>Article: {article.paragraph}</h1>
     </div>
   );
 };
