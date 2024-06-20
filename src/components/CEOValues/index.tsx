722551bf9d263a1dada978f78d18e92ffda542d0
Lisette Svan <svanlisette@gmail.com>
2024-06-20 19:03:35 +0200

    made everything, just language left

---
+++ src/components/CEOValues/index.tsx
@@ -0,0 +1,30 @@
+import { useTranslations } from 'next-intl';
+import React from 'react';
+
+const CEOValues = () => {
+    const t = useTranslations("")
+    return (
+        <>
+            <div className='bg-pt-background'>
+                <div className='text-center'>
+                    <h1 className='md:text-5xl text-4xl pt-[12%] md:pt-[5%] font-normal font-nunito'>Our Values</h1>
+                    <p className='md:mt-[2%] font-normal hidden md:block'>What we believe in</p>
+                    <p className='mt-[2%] md:hidden'>The things we believe in</p>
+                </div>
+                <div className='flex justify-center flex-col md:flex-row w-[100%] md:w[100%] items-center'>
+                    <img className='p-[4%]' src="/Simplicity.svg" alt="simplicity image" />
+                    <img src="/Innovation.svg" alt="innovation image" />
+                    <img className='p-[4%]' src="/User.svg" alt="User friendly image" />
+                </div>
+            </div>
+            <div className='mt-[8%] ml-[25%] mb-8 w-3/6 italic text-center flex justify-center align-center font-nunito font-light text-xl'>
+                <p>“Of all the things you need to think about during the day, parking should be one of the least. Parking should be so seamless that you hardly need to think about it; it should just work. We call this Smart Parking."</p>
+            </div>
+            <div>
+                <p className='italic mb-[8%] text-center flex justify-center font-nunito font-light text-xl'>- Matilda, CEO and Founder.</p>
+            </div>
+        </>
+    )
+}
+
+export default CEOValues;
