dab8d5919c2891d1073fbe58cf83edda01741576
Lisette Svan <svanlisette@gmail.com>
2024-06-25 10:13:09 +0200

    changed in language file

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,23 +1,17 @@
 import React from 'react';
 import AboutHero from '@/components/AboutHero';
 import KnowMore from '../../../components/KnowMore';
-<<<<<<< HEAD
 import MeetTheTeam from '@/components/MeetTheTeam';
 import CEOValues from '@/components/CEOValues';
-=======
 import OurVisionStory from '@/components/OurVisionStory';
->>>>>>> PTS-31-Vision-Story
 
 const About = () => {
     return (
         <div>
             <AboutHero />
-<<<<<<< HEAD
             <CEOValues />
             <MeetTheTeam />
-=======
             <OurVisionStory />
->>>>>>> PTS-31-Vision-Story
             <KnowMore />
         </div>
     );
