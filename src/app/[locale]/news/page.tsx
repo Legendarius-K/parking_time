9008044f40eb2b4fce9b0d5783ce427399baaba1
elafalzoubi <elafalzoubi@example.com>
2024-06-25 09:47:07 -0700

    changed the color of the selection

--- src/app/[locale]/news/page.tsx
+++ src/app/[locale]/news/page.tsx
@@ -131,7 +131,7 @@ const News = () => {
                         <button
                             id="multiLevelDropdownButton"
                             onClick={toggleMultiDropdown}
-                            className="text-black bg-[#ffffff] hover:bg-[#e2e2e2] focus:ring-4 focus:outline-none focus:ring-blue-300 shadow-md font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                            className="text-black bg-[#ffffff] hover:bg-[#e2e2e2] focus:ring-4 focus:outline-none focus:ring-blue-300 shadow-md font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-[#ffffff] dark:hover:bg-[#e2e2e2]  dark:focus:ring-[#ffffff]"
                             type="button"
                         >
                             <p>{sortOrder ? t(sortOrder) : t('Select')}</p>
